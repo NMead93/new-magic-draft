1bf3e1058ac9e813e0227fc15ede480268bdee99
Nicholas Mead <nixter11@uw.edu>
2017-04-11 19:58:06 -0700

    add set options

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -67,7 +67,7 @@ export class HomeComponent {
 }, 5000);
 }
 
-  
+
 
 
   showme() {
---
+++ src/app/player-info/player-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-info',
+  templateUrl: './player-info.component.html',
+  styleUrls: ['./player-info.component.css']
+})
+export class PlayerInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
