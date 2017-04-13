74032e6852d1e2095e112801fb8b5e29073c801b
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-04-13 10:20:08 -0700

    more work on draft end

--- src/app/draft-end/draft-end.component.ts
+++ src/app/draft-end/draft-end.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MagicService } from '../magic.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-draft-end',
@@ -8,12 +10,40 @@ import { MagicService } from '../magic.service';
   providers: [MagicService]
 })
 export class DraftEndComponent implements OnInit {
-  constructor(private magicService: MagicService) { }
+  constructor(private magicService: MagicService, private route: ActivatedRoute, private location: Location) { }
+
+  //global variables
+  currentDraftId: string;
+  currentDraft;
+  playerArray = [];
+
+  //global variables
 
   ngOnInit() {
+    this.route.params.forEach(urlParameters => {
+      this.currentDraftId = urlParameters['id'];
+    });
+    this.magicService.getDraft(this.currentDraftId).subscribe(data => {
+      this.currentDraft = data;
+      console.log(this.currentDraft);
+      this.setArrayOfPlayers(this.currentDraft);
+    });
   }
 
+  setArrayOfPlayers(draft) {
+    for(let i = 0; i < draft.players.length; i++){
+      this.playerArray.push(draft.players[i]);
+    }
+    console.log(this.playerArray);
+  }
 
+  setArray(cards){
+    let output: any[] = [];
+    for(let i = 0; i < cards.length; i++) {
+      output.push(cards[i]);
+    }
+    return output;
+  }
 
 
 }
